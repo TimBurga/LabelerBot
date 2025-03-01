172794b9c685f8dcacd956fe2245e03716664c3f
Tim Burga <timburga@gmail.com>
2025-02-28 19:12:42 -0600

    Refresh authenticated session before expiration (maybe)

--- AtProtoSessionManager.cs
+++ AtProtoSessionManager.cs
@@ -1,5 +1,6 @@
 ﻿using FishyFlip;
 using FishyFlip.Models;
+using Timer = System.Timers.Timer;
 
 namespace LabelerBot;
 
@@ -14,6 +15,7 @@ public class AtProtoSessionManager : IAtProtoSessionManager
     private readonly ATDid _labelerDid;
     private readonly string _labelerPassword;
     private readonly ATProtocol _atproto;
+    private Timer _refreshTimer;
 
     public AtProtoSessionManager(IConfiguration config, ILogger<AtProtoSessionManager> logger)
     {
@@ -21,6 +23,31 @@ public class AtProtoSessionManager : IAtProtoSessionManager
         _labelerDid = ATDid.Create(config.GetValue<string>("Labeler:Did")!)!;
         _labelerPassword = config.GetValue<string>("Labeler:Password")!;
         _atproto = new ATProtocolBuilder().WithOzoneProxy(_labelerDid).Build();
+        _refreshTimer = new Timer();
+        _refreshTimer.Elapsed += async (sender, args) => await RefreshSession();
+    }
+
+    private async Task RefreshSession()
+    {
+        _logger.LogInformation("Timer elapsed - refreshing authenticated session");
+
+        var session = await _atproto.RefreshAuthSessionAsync();
+        if (session == null)
+        {
+            _logger.LogError("Failed to refresh session - trying new session");
+            var (newSession, error) = await _atproto.AuthenticateWithPasswordResultAsync(_labelerDid.Handler, _labelerPassword);
+            if (error != null)
+            {
+                _logger.LogCritical("Failed to get new session: {error}", error);
+                return;
+            }
+
+            _logger.LogInformation("Got new session: {error}", error);
+            return;
+        }
+
+        _refreshTimer.Interval = session.Session.ExpiresIn.Subtract(DateTime.Now).Add(TimeSpan.FromSeconds(10)).TotalMilliseconds;
+        _refreshTimer.Start();
     }
 
     public async Task<ATProtocol> GetSession()
@@ -32,11 +59,8 @@ public class AtProtoSessionManager : IAtProtoSessionManager
                 _logger.LogDebug("Authenticating as {did}", _labelerDid.Handler);
                 var (session, error) = await _atproto.AuthenticateWithPasswordResultAsync(_labelerDid.Handler, _labelerPassword);
 
-                if (session is null)
-                {
-                    _logger.LogInformation("Refreshing authenticated session");
-                    await _atproto.RefreshAuthSessionAsync();
-                }
+                _refreshTimer.Interval = session.ExpiresIn.Subtract(DateTime.Now).Add(TimeSpan.FromSeconds(10)).TotalMilliseconds;
+                _refreshTimer.Start();
 
                 _logger.LogDebug("Got new session {jwt}", session.AccessJwt);
             }
@@ -49,6 +73,5 @@ public class AtProtoSessionManager : IAtProtoSessionManager
         }
 
         return _atproto;
-
     }
 }
